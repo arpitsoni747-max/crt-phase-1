2e9d485cd1578a7fb5a9e48c9cf41d6429aec9b6
Arpit Soni <your-email@example.com>
2026-05-04 11:10:12 +0530

    commit

--- day 4/code3.cpp
+++ day 4/code3.cpp
@@ -5,11 +5,9 @@ int main() {
     int n = 4;
 
     for(int i = 1; i <= n; i++) {
-        // spaces
         for(int j = 1; j <= n - i; j++) {
             cout << " ";
         }
-        // stars
         for(int k = 1; k <= i; k++) {
             cout << "*";
         }
