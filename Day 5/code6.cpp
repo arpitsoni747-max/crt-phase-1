97c3b6a21a0604a5b5451fe990653c519051e1b4
Arpit Soni <your-email@example.com>
2026-05-05 10:28:00 +0530

    commit

---
+++ Day 5/code6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int value = 10;
+    int *ptr = &value;
+    cout <<"add:"<<ptr << endl;
+    cout <<"value:"<<*ptr<<endl;
+
+    *ptr = 20;
+    cout<<"value:"<<*ptr<<endl;
+    return 0;
+}
