6497718ad4b69cffb7f7957a0afc2b0928f19d97
Arpit Soni <your-email@example.com>
2026-05-06 08:41:42 +0530

    commit

---
+++ day 2/abcd1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int age;
+
+    cout << "Enter age: ";
+    cin >> age;
+
+    bool discount = (age < 12) || (age >= 65);
+    bool working = (age >= 18) && (age <= 64);
+
+    cout << "Discount Eligible: " << discount << endl;
+    cout << "Working Age: " << working << endl;
+
+    return 0;
+}
