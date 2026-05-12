fc8a7020a0a0858a6355a271691a19c80086f0c0
Arpit Soni <your-email@example.com>
2026-05-12 09:19:11 +0530

    commit

---
+++ day 12/selection_sorting.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+void swap_array(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void Selection_Sort(int nums[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (nums[j] < nums[min_index])
+            {
+                min_index = j;
+            }
+        }
+        swap_array(nums[i], nums[min_index]);
+    }
+}
+int main()
+{
+    int arr[5] = {1, 2, 3, 5, 4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    Selection_Sort(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
